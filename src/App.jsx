d57919d2d1b857c4b373428e1d74edeceb86b2bb
SriJan <srijan1769@gmail.com>
2026-01-11 19:14:50 +0545

    Added new logo and made some minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import './assets/stock.svg';
+
 import "./App.css";
 
 import Login from "./pages/Login.jsx";
