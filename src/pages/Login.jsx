e7bc861b689a3de851357f778f7432d0531720c6
SriJan <srijan1769@gmail.com>
2026-01-18 19:34:47 +0545

    Modified the login credentials

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -12,60 +12,65 @@ function Login() {
       toast.error("Please fill in all fields");
       return;
     }
-    // Add login logic here
-    toast.success("Login successful");
-  }
+    if (Email == "admin@ipo.com" && Password == "admin123"){
+      toast.success("Admin Login successful");
+    }
+    else{
+      toast.error("Access Denied: Invalid credentials");
+    }
+   
+    // toast.success("Login successful");
+  };
 
   return (
     <>
-    
-    <div className="login-container">
-      <div className="login-header">
-        <div className="login-logo">
-          <img src={Logo} alt="IPO-Varum Logo" width={30} height={30} />
-          &nbsp; IPO-Varum
-        </div>
-        <button className="support-button">Talk to Support</button>
-      </div>
-
-     <ToastContainer position="top-right" autoClose={3000} />
-
-
-      <div className="login-title">Log in to Your Account</div>
-
-      <div className="login-card">
-        <div className="login-label">Email Address</div>
-        <input
-          type="text"
-          placeholder="Email"
-          value={Email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-
-        <div className="login-label">Password</div>
-        <input
-          type="password"
-          placeholder="Password"
-          value={Password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button className="login-button" onClick={handleLogin}>Login</button>
-
-        <div className="login-options">
-          <Link to="/forget-password" className="forgot-password">
-            Forgot Password?
-          </Link>
+      <div className="login-container">
+        <div className="login-header">
+          <div className="login-logo">
+            <img src={Logo} alt="IPO-Varum Logo" width={30} height={30} />
+            &nbsp; IPO-Varum
+          </div>
+          <button className="support-button">Talk to Support</button>
         </div>
 
-      </div>
+        <ToastContainer position="top-right" autoClose={3000} />
 
-      <footer className="footer-content">
-        <p className="footer-copyright">
-          &copy; {new Date().getFullYear()} SriJan Karki. All rights reserved.
-        </p>
-      </footer>
-    </div>
-  </>
+        <div className="login-title">Log in to Your Account</div>
+
+        <div className="login-card">
+          <div className="login-label">Email Address</div>
+          <input
+            type="text"
+            placeholder="Email"
+            value={Email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <div className="login-label">Password</div>
+          <input
+            type="password"
+            placeholder="Password"
+            value={Password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button className="login-button" onClick={handleLogin}>
+            Login
+          </button>
+
+          <div className="login-options">
+            <Link to="/forget-password" className="forgot-password">
+              Forgot Password?
+            </Link>
+          </div>
+        </div>
+
+        <footer className="footer-content">
+          <p className="footer-copyright">
+            &copy; {new Date().getFullYear()} SriJan Karki. All rights reserved.
+          </p>
+        </footer>
+      </div>
+    </>
   );
 }
 
